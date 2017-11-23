670977ea389c116a9f1ff7d6eb631d8bb96f3b6a
Roman Mátyus <romanmatyus@gmail.com>
2017-11-23 12:52:15 +0100

    Fix replace image
    Replace real address, not for example `//` as before.

--- src/RM/AssetsCollector/Compilers/ImageReplacer.php
+++ src/RM/AssetsCollector/Compilers/ImageReplacer.php
@@ -36,7 +36,7 @@ class ImageReplacer extends BaseCssAssetsCompiler implements IAssetsCompiler
 					$output_file = $this->webTemp."/".$hash.".".$ext;
 					if (!file_exists($output_file))
 						copy($source_file,$output_file);
-					$this->output = str_replace($img,substr($output_file,strlen(realpath($this->wwwDir))),$this->output);
+					$this->output = str_replace($img,substr(realpath($output_file),strlen(realpath($this->wwwDir))),$this->output);
 				}
 			}
 		}
